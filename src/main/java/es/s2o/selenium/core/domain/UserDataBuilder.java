587d49d3f302916eb504dbc3c86f444ae0acbacb
s2oBCN <sergio.sacristan@boehringer-ingelheim.com>
2022-04-27 12:20:46 +0200

    initial commit

---
+++ src/main/java/es/s2o/selenium/core/domain/UserDataBuilder.java
@@ -0,0 +1,9 @@
+package es.s2o.selenium.core.domain;
+
+public class UserDataBuilder {
+
+    public static UserBuilder defaultUser(){
+        return UserBuilder.anUser().withUserName("tomsmith").withPassword("SuperSecretPassword!");
+    }
+
+}
